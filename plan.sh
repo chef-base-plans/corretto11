104ab919bd450e1ec6f112ba64ad035dd02037be
rybons <bonnerrk@gmail.com>
2021-03-17 18:15:09 -0400

    Bumped version 11.0.2.9.3 -> 11.0.10.9.1

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=corretto11
 pkg_origin=core
-pkg_version=11.0.2.9.3
+pkg_version=11.0.10.9.1
 pkg_description=('Corretto is a build of the Open Java Development Kit (OpenJDK) with long-term support from Amazon.')
 pkg_license=("GPL-2.0-only")
 pkg_upstream_url=https://aws.amazon.com/corretto/
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_source=https://d3pxv6yz143wms.cloudfront.net/${pkg_version}/amazon-corretto-${pkg_version}-linux-x64.tar.gz
-pkg_shasum=be8452a78baa077e19afbfa64070b26275030267a5ea4e837dec7a30eff85c9c
+pkg_shasum=bf9380ee0cdd78fafb6d0cdfa0c1a97baaaec44432a15c8c2f296696ad9ed631
 pkg_dirname="amazon-corretto-${pkg_version}-linux-x64"
 pkg_filename="corretto-${pkg_version}_linux-x64_bin.tar.gz"
 pkg_deps=(
